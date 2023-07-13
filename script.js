d32c4ce2b2be9eddd4de7acf9b59565950adf5ac
Mike <revolutiondaniel19@gmail.com>
2023-07-13 18:46:16 -0500

    plantilla

---
+++ script.js
@@ -0,0 +1,9 @@
+const days = document.getElementById("days"),
+      hours = document.getElementById("hours"),
+      min = document.getElementById("min"),
+      sec = document.getElementById("sec");
+
+
+
+
+
